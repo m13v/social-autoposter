6edcfe27293b424f4e9fbc86262cbebb474af0d9
m13v <i@m13v.com>
2026-04-17 16:50:55 -0700

    Add systemd scheduler backend stub

---
+++ bin/scheduler/systemd.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function generate() {
+  throw new Error('systemd scheduler backend not yet implemented');
+}
+
+function defaultEnv() {
+  throw new Error('systemd scheduler backend not yet implemented');
+}
+
+module.exports = { generate, defaultEnv };
